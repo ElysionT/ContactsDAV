c578248cd399af62473781b562005ca27be1cff5
tianfeng3 <tianfeng3@lenovo.com>
2015-12-29 10:10:16 +0800

    [master][davdroid]Init davdroid

---
+++ app/src/main/java/at/bitfire/davdroid/Constants.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2013 – 2015 Ricki Hirner (bitfire web engineering).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ */
+package at.bitfire.davdroid;
+
+import android.net.Uri;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String
+		ACCOUNT_TYPE = "bitfire.at.davdroid",
+		WEB_URL_MAIN = "https://davdroid.bitfire.at/?pk_campaign=davdroid-app",
+		WEB_URL_HELP = "https://davdroid.bitfire.at/configuration?pk_campaign=davdroid-app";
+
+    public static final Logger log = LoggerFactory.getLogger("davdroid");
+
+    // notification IDs
+    public final static int
+            NOTIFICATION_ANDROID_VERSION_UPDATED = 0,
+            NOTIFICATION_ACCOUNT_SETTINGS_UPDATED = 1,
+            NOTIFICATION_CONTACTS_SYNC = 10,
+            NOTIFICATION_CALENDAR_SYNC = 11,
+            NOTIFICATION_TASK_SYNC = 12;
+
+    public final static Uri donationUri = Uri.parse("https://davdroid.bitfire.at/donate?pk_campaign=davdroid-app");
+}
